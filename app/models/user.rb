4842752f42d41ec84e9230ae55b90035a378aeb2
Sergio Filho Garcia Paim <sergio.filho@outlook.com>
2026-03-30 18:25:31 +0000

    feat: definição estruturas dependência entre classes do domínio

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  has_many :posts, class_name: "Post", dependent: :destroy
+  has_many :comments, class_name: "Comment", dependent: :destroy
+  has_many :messages, class_name: "Message", dependent: :destroy
+  has_many :sessions, class_name: "Session", dependent: :destroy
 end
